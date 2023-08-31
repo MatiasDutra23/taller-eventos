7ca0f4b850a940c7985a1d7a46fb0dfe8e7ccaa6
Matias Dutra <114629823+MatiasDutra23@users.noreply.github.com>
2023-08-31 12:38:22 -0300

    solucion-click
    event.stopPropagation()

--- eventos.js
+++ eventos.js
@@ -1,12 +1,23 @@
-const BtnSaludar = document.getElementById("zanahoriasVerdes");
+// eventos.js
+// Obtenemos referencias a los elementos del DOM
+const div = document.getElementById("zanahoriasVerdes");
+const btnSaludar = document.getElementById("btnSaludar");
 
+// Función que muestra un mensaje de alerta cuando se hace clic en el div
 function saludarDiv(){
-    window.alert('Hola! Soy el div');
-}
-function saludar(){
-    window.alert('Hola!');
+  window.alert('Hola! Soy el div');
 }
 
-BtnSaludar.addEventListener("click", () => {
-    saludarDiv();
+// Función que muestra un mensaje de alerta cuando se hace clic en el botón "Saludar"
+function saludar(event){
+  window.alert('Hola!');
+}
+
+// Agregamos un evento al div para que se active cuando se haga clic en él
+div.addEventListener("click", saludarDiv);
+
+// Agregamos un evento al botón "Saludar"
+btnSaludar.addEventListener("click", (event) => {
+  event.stopPropagation(); // Detiene la propagación del evento actual
+  saludar(); // Llama a la función saludar
 });
